c9cb0272dc001b31c5cf25f48b10ed4f548a48d3
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-08-17 15:46:58 -0400

    Index, New, Create methods added with corresponding views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,20 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params.require(:project).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: 'Project is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
