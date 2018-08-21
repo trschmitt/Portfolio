a83116ca8c28d8733d950d4d78aa70014da6a864
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-08-21 14:17:37 -0400

    Customized routes to add root page and about/contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   resources :projects
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
-  
+
+  root to: 'pages#home'
 end
