4c07035e57ca06c6c58d8542a9e10298d5573aad
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-08-23 16:13:07 -0400

    Added title and body validation to Blog.rb

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,8 +2,10 @@ class Blog < ApplicationRecord
   # set status to Draft or Published so that you haver two different statuses to show only published blogs
   enum status: { draft: 0, published: 1}
 
-  # Integration of Friendly_ID 
+  # Integration of Friendly_ID
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
 
 end
