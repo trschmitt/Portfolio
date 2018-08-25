a86aa958a8269706aeae9085c28a4cc9254c7320
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-08-25 11:07:34 -0400

    Implemented image defaults for new Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,13 @@ class Project < ApplicationRecord
 
   scope :angular_js_projects, -> {where(subtitle: 'Angular.js')}
   scope :ruby_on_rails_projects, -> {where(subtitle: 'Ruby on Rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    # If images are empty set to default. If there is an image there already it won't overwrite it. If nil set if not nil skip. key is the double pipes ||
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
+
 end
