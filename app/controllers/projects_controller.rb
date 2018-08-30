888ce94ffe6556ae4b0727035ae6a295ad0fdecd
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-08-30 15:29:06 -0400

    Implemented nested form for Tehcnoliges Used on Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+    3.times { @project.technologies.build }
   end
 
   def edit
@@ -21,7 +22,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(params.require(:project).permit(:title, :subtitle, :body))
+    @project = Project.new(params.require(:project).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @project.save
