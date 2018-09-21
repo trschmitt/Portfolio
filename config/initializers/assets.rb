127615431f79e92b3222a44a7ba6de49b88954cd
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-09-21 13:58:11 -0400

    Added layouts for Blogs and Projects

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Blogs Styling precompile line
+Rails.application.config.assets.precompile += %w( blogs.scss )
+
+# Projects Styling Precompile Line
+Rails.application.config.assets.precompile += %w( projects.scss )
